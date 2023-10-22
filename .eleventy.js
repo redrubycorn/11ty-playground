7fc935ea9dbbffad4da0548a3ed196de264b7044
redrubycorn <redrubycorn@gmail.com>
2023-10-22 23:37:20 +0200

    Change layouts and eleventy.js
    Change the layouts of the blog and posts.
    Add layouts for projects.
    Change the .eleventy.js to add new collections and function to get the functionality to have the tags for projects and posts separated.

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ const readingTime = require("eleventy-plugin-reading-time");
 const { DateTime } = require("luxon");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
+
+
+
+
 module.exports = eleventyConfig => {
 
     // added yaml support
@@ -18,6 +22,8 @@ module.exports = eleventyConfig => {
         }).setLocale('en').toLocaleString(DateTime.DATE_FULL);
     });
 
+    eleventyConfig.addFilter("filterTagList", filterTagList);
+
     
     // ---------------------------------
     // PLUGINS
@@ -30,31 +36,127 @@ module.exports = eleventyConfig => {
 
 
     // ---------------------------------
+    // FUNCTIONS
+    // ---------------------------------
+
+    // filter out certain tags, e.g. utility tags
+    function filterTagList(tags) {
+        return (tags || []).filter(
+            (tag) => ["utility-tag", "unwanted-tag"].indexOf(tag) === -1
+        );
+    };
+
+    // return a list of tags in a given collection
+    function getTagList(collection) {
+        let tagSet = new Set();
+        collection.forEach((item) => {
+            (item.data.tags || []).forEach((tag) => tagSet.add(tag));
+        });
+        return filterTagList([...tagSet]);
+    };
+
+    // return an object with arrays of posts by tag from the provided collections
+    function createCollectionsByTag(collection) {
+        // set the result as an object
+        let resultArrays = {};
+        // loop through each item in the provided collection
+        collection.forEach((item) => {
+            // loop through the tags of each item
+            item.data.tags.forEach((tag) => {
+                // if the tag has not already been added to the object, add it as an empty array
+                if (!resultArrays[tag]) { resultArrays[tag] = []; }
+                // add the item to the tag's array
+                resultArrays[tag].push(item);
+            });
+        });
+        // return the object containing tags and their arrays of posts
+        // { tag-name: [post-object, post-object], tag-name: [post-object, post-object] }
+        return resultArrays;
+    };
+
+    // Return a list of years in a given collection
+    function getYearList(collection) {
+        let years = new Set();
+        collection.forEach((item) => {
+            years.add(
+                DateTime.fromJSDate(item.data.date, { zone: "utc" }).toFormat("yyyy")
+            );
+        });
+        return [...years];
+    }
+
+    // Return an object with arrays of posts by year from the provided collection
+    function createCollectionsByYear(collection) {
+        // set the result as an object
+        let resultArrays = {};
+        // loop through each item in the provided collection
+        collection.forEach((item) => {
+            year = DateTime.fromJSDate(item.data.date, { zone: "utc" }).toFormat(
+                "yyyy"
+            );
+            if (!resultArrays[year]) {
+                resultArrays[year] = [];
+            }
+            resultArrays[year].push(item);
+        });
+        // Return the object containing tags and their arrays of posts
+        // { year: [post-object, post-object], year: [post-object, post-object] }
+        return resultArrays;
+    }
+
+
+    // ---------------------------------
     // COLLECTIONS
     // ---------------------------------
 
+    // PROJECTS
+    // Get a list of all posts with collections.posts
+    eleventyConfig.addCollection("projects", function (collectionAPI) {
+        return collectionAPI.getFilteredByGlob("./src/content/projects/**/*.md");
+    });
+    
+    // Get a list of all tags used by posts with collections.postTags
+    eleventyConfig.addCollection("projectTags", function (collectionAPI) {
+        let collection = collectionAPI.getFilteredByGlob("./src/content/projects/**/*.md");
+        return getTagList(collection);
+    });
+
+    // Get a list of all posts with a specific tag with collections.postsTagged["tag"]
+    eleventyConfig.addCollection("projectsTagged", function (collectionAPI) {
+        let collection = collectionAPI.getFilteredByGlob("./src/content/projects//**/*.md");
+        return createCollectionsByTag(collection);
+    });
+
+    
     // POSTS
-    eleventyConfig.addCollection('blog', collection => {
-        return [...collection.getFilteredByGlob('./src/content/posts/**/*.md')].reverse();
+    // Get a list of all posts with collections.posts
+    eleventyConfig.addCollection("posts", function (collectionAPI) {
+        return collectionAPI.getFilteredByGlob("./src/content/posts/**/*.md");
+    });
+    
+    // Get a list of all tags used by posts with collections.postTags
+    eleventyConfig.addCollection("postTags", function (collectionAPI) {
+        let collection = collectionAPI.getFilteredByGlob("./src/content/posts/**/*.md");
+        return getTagList(collection);
     });
 
-    // TAGS
-    eleventyConfig.addCollection('tagList', collection => {
-        const tagsSet = {};
-        collection.getAll().forEach(item => {
-            if (!item.data.tags) return;
-            item.data.tags.filter(
-                tag => !['posts', 'all'].includes(tag)
-            ).forEach(
-                tag => {
-                    if (!tagsSet[tag]) { tagsSet[tag] = []; }
-                    tagsSet[tag].push(item)
-                }
-            );
-        });
-        return tagsSet;
+    // Get a list of all posts with a specific tag with collections.postsTagged["tag"]
+    eleventyConfig.addCollection("postsTagged", function (collectionAPI) {
+        let collection = collectionAPI.getFilteredByGlob("./src/content/posts/**/*.md");
+        return createCollectionsByTag(collection);
     });
 
+    // POSTS YEARS
+    // Create a collection for post years (required for the generation of archive pages)
+    eleventyConfig.addCollection("postYears", function (collectionAPI) {
+        let collection = collectionAPI.getFilteredByGlob("./src/content/posts/**/*.md");
+        return getYearList(collection);
+    });
+    // Create a collection for posts by year
+    eleventyConfig.addCollection("postsByYear", function (collectionAPI) {
+        let collection = collectionAPI.getFilteredByGlob("./src/content/posts/**/*.md");
+        return createCollectionsByYear(collection);
+    });
 
     // ---------------------------------
     // PASSTHROUGH
